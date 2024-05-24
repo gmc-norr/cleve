230b941dcda08aefc245e2a7dccba74d85ee4a72
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-05-24 13:35:38 +0200

    Basic SampleSheet parsing

---
+++ samplesheet.go
@@ -0,0 +1,271 @@
+package cleve
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type SectionType = int
+
+const (
+	UnknownSection SectionType = iota
+	SettingsSection
+	DataSection
+)
+
+type SampleSheet struct {
+	Sections []Section
+}
+
+func (s SampleSheet) Section(name string) *Section {
+	for _, section := range s.Sections {
+		if section.Name == name {
+			return &section
+		}
+	}
+	return nil
+}
+
+func (s SampleSheet) Version() int {
+	v, _ := s.Section("Header").GetInt("FileFormatVersion")
+	return v
+}
+
+func (s SampleSheet) IsValid() bool {
+	return s.Section("Header") != nil && s.Section("Reads") != nil
+}
+
+type Section struct {
+	Name string
+	Type int
+	Rows [][]string
+}
+
+func (s Section) Get(name string, index ...int) (string, bool) {
+	switch s.Type {
+	case SettingsSection:
+		for _, row := range s.Rows {
+			if row[0] == name {
+				return row[1], true
+			}
+		}
+	case DataSection:
+		colIndex := -1
+		for i, s := range s.Rows[0] {
+			if s == name {
+				colIndex = i
+				break
+			}
+		}
+
+		rowIndex := -1
+		if len(index) > 0 {
+			rowIndex = index[0]
+		} else {
+			return "", false
+		}
+
+		if colIndex >= 0 && rowIndex >= 0 {
+			return s.Rows[rowIndex][colIndex], true
+		}
+
+	}
+
+	return "", false
+}
+
+func (s Section) GetInt(name string, index ...int) (int, error) {
+	v, ok := s.Get(name, index...)
+	if !ok {
+		if index != nil {
+			return 0, fmt.Errorf("index %d for key %s not found", index[0], name)
+		}
+		return 0, fmt.Errorf("key %s not found", name)
+	}
+	return strconv.Atoi(v)
+}
+
+func (s Section) GetFloat(name string, index ...int) (float64, error) {
+	v, ok := s.Get(name, index...)
+	if !ok {
+		if index != nil {
+			return 0, fmt.Errorf("index %d for key %s not found", index[0], name)
+		}
+		return 0, fmt.Errorf("key %s not found", name)
+	}
+	return strconv.ParseFloat(v, 64)
+}
+
+type sampleSheetParser struct {
+	reader *bufio.Reader
+}
+
+func (p *sampleSheetParser) Peek() (rune, error) {
+	r, _, err := p.reader.ReadRune()
+	p.reader.UnreadRune()
+	return r, err
+}
+
+func (p *sampleSheetParser) ParseHeader() (string, int, error) {
+	r, _, _ := p.reader.ReadRune()
+	if r == '\x00' {
+		return "", UnknownSection, io.EOF
+	}
+	if r != '[' {
+		return "", UnknownSection, fmt.Errorf("parsing error: expected section header")
+	}
+	name := ""
+	for {
+		r, _, err := p.reader.ReadRune()
+		if err != nil {
+			return "", UnknownSection, err
+		}
+		if r == ']' {
+			if len(name) == 0 {
+				return "", UnknownSection, fmt.Errorf("parsing error: empty section header")
+			}
+			// check that the rest of the line is empty
+			rest, err := p.reader.ReadBytes('\n')
+			if err != nil && err.Error() != "EOF" {
+				return "", UnknownSection, err
+			}
+			restTrim := strings.TrimSpace(string(rest))
+			if len(restTrim) > 0 {
+				// Allow trailing commas
+				for _, rune := range restTrim {
+					if rune != ',' {
+						return "", UnknownSection, fmt.Errorf(
+							"parsing error: trailing characters after header %q",
+							string(name),
+						)
+					}
+				}
+			}
+			break
+		}
+		name += string(r)
+	}
+	var sectionType int
+	switch {
+	case name == "Header", name == "Reads":
+		sectionType = SettingsSection
+	case name == "Data":
+		sectionType = DataSection
+	case strings.HasSuffix(strings.ToLower(name), "data"):
+		sectionType = DataSection
+	case strings.HasSuffix(strings.ToLower(name), "settings"):
+		sectionType = SettingsSection
+	default:
+		return name, UnknownSection, fmt.Errorf("unknown section type: %s", name)
+	}
+	return name, sectionType, nil
+}
+
+// Parse a row of a section in the sample sheet.
+// If the row is empty, or end of file is reached and no values
+// have been read, then nil is returned for both the value and the error.
+func (p *sampleSheetParser) ParseSectionRow() ([]string, error) {
+	values := []string{}
+	currentValue := ""
+	if c, _ := p.Peek(); c == '[' {
+		return nil, fmt.Errorf("parsing error: empty section")
+	}
+	for {
+		r, _, err := p.reader.ReadRune()
+		if err != nil {
+			if err.Error() == "EOF" {
+				if currentValue != "" {
+					values = append(values, currentValue)
+				}
+				if len(values) > 0 {
+					// eof on the last line of a section, that's ok
+					return values, nil
+				}
+				// empty line at the end of the file, that's ok
+				return nil, nil
+			}
+			return values, err
+		}
+		switch r {
+		case '\n':
+			if currentValue == "" && len(values) == 0 {
+				// Empty line
+				return nil, nil
+			}
+			values = append(values, currentValue)
+			return values, nil
+		case ',':
+			values = append(values, currentValue)
+			currentValue = ""
+			continue
+		}
+		currentValue += string(r)
+	}
+}
+
+func ParseSampleSheet(r *bufio.Reader) (SampleSheet, error) {
+	p := sampleSheetParser{r}
+	sheet := SampleSheet{}
+	for {
+		name, typ, err := p.ParseHeader()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			return sheet, err
+		}
+
+		var s Section
+		s.Type = typ
+		s.Name = name
+
+		for {
+			values, err := p.ParseSectionRow()
+			if err != nil {
+				if err.Error() == "EOF" {
+					break
+				}
+				return sheet, err
+			}
+			
+			if values == nil {
+				break
+			}
+
+			s.Rows = append(s.Rows, values)
+
+			next, err := p.Peek()
+			if err != nil {
+				if err.Error() == "EOF" {
+					break
+				}
+				return sheet, err
+			}
+			if next == '[' {
+				// New section
+				break
+			}
+		}
+
+		if len(s.Rows) == 0 {
+			return sheet, fmt.Errorf("parsing error: empty section")
+		}
+
+		rowItemCount := len(s.Rows[0])
+		if s.Type == SettingsSection && rowItemCount != 2 {
+			return sheet, fmt.Errorf("parsing error: expected 2 items per row in section %q", s.Name)
+		}
+		for _, row := range s.Rows {
+			if len(row) != rowItemCount {
+				return sheet, fmt.Errorf("parsing error: expected %d items per row in section %q", rowItemCount, s.Name)
+			}
+		}
+
+		sheet.Sections = append(sheet.Sections, s)
+	}
+
+	return sheet, nil
+}
---
+++ samplesheet_test.go
@@ -0,0 +1,387 @@
+package cleve
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+)
+
+func TestSampleSheet(t *testing.T) {
+	cases := []struct {
+		Name                  string
+		Version               int
+		Valid                 bool
+		Sections              []string
+		SectionRows           []int
+		SectionTypes          []SectionType
+		Data                  []byte
+		ExpectedSettingValues map[string]map[string]string
+		ExpectedDataValues    map[string]map[string]map[int]string
+	}{
+		{
+			"case 1",
+			2,
+			false,
+			[]string{"Header"},
+			[]int{4},
+			[]SectionType{SettingsSection},
+			[]byte(`[Header]
+FileFormatVersion,2
+RunName,TestRun
+InstrumentPlatform,NovaSeq
+InstrumentType,NovaSeq X Plus`),
+			map[string]map[string]string{
+				"Header": {
+					"FileFormatVersion":  "2",
+					"RunName":            "TestRun",
+					"InstrumentPlatform": "NovaSeq",
+					"InstrumentType":     "NovaSeq X Plus",
+				},
+			},
+			nil,
+		},
+		{
+			"case 2",
+			2,
+			true,
+			[]string{"Header", "Reads"},
+			[]int{4, 4},
+			[]SectionType{SettingsSection, SettingsSection},
+			[]byte(`[Header]
+FileFormatVersion,2
+RunName,TestRun
+InstrumentPlatform,NovaSeq
+InstrumentType,NovaSeq X Plus
+[Reads]
+Read1Cycles,151
+Index1Cycles,8
+Index2Cycles,8
+Read2Cycles,151`),
+			map[string]map[string]string{
+				"Reads": {
+					"Read1Cycles":  "151",
+					"Index1Cycles": "8",
+					"Index2Cycles": "8",
+					"Read2Cycles":  "151",
+				},
+			},
+			nil,
+		},
+		{
+			"case 3",
+			2,
+			true,
+			[]string{"Header", "Reads", "Data", "Sequencing_Settings"},
+			[]int{4, 4, 5, 1},
+			[]SectionType{SettingsSection, SettingsSection, DataSection, SettingsSection},
+			[]byte(`[Header]
+FileFormatVersion,2
+RunName,TestRun
+InstrumentPlatform,NovaSeq
+InstrumentType,NovaSeq X Plus
+
+[Reads]
+Read1Cycles,151
+Read2Cycles,151
+Index1Cycles,8
+Index2Cycles,8
+
+[Data]
+col1,col2,col3
+val1.1,val2.1,val3.1
+val1.2,val2.2,val3.2
+val1.3,val2.3,val3.3
+val1.4,val2.4,val3.4
+
+[Sequencing_Settings]
+LibraryPrepkits,prepkit1;prepkit2`),
+			map[string]map[string]string{
+				"Header": {
+					"FileFormatVersion":  "2",
+					"RunName":            "TestRun",
+					"InstrumentPlatform": "NovaSeq",
+					"InstrumentType":     "NovaSeq X Plus",
+				},
+				"Reads": {
+					"Read1Cycles":  "151",
+					"Read2Cycles":  "151",
+					"Index1Cycles": "8",
+					"Index2Cycles": "8",
+				},
+				"Sequencing_Settings": {
+					"LibraryPrepkits": "prepkit1;prepkit2",
+				},
+			},
+			map[string]map[string]map[int]string{
+				"Data": {
+					"col1": {
+						1: "val1.1",
+						2: "val1.2",
+						3: "val1.3",
+						4: "val1.4",
+					},
+					"col2": {
+						1: "val2.1",
+						2: "val2.2",
+						3: "val2.3",
+						4: "val2.4",
+					},
+					"col3": {
+						1: "val3.1",
+						2: "val3.2",
+						3: "val3.3",
+						4: "val3.4",
+					},
+				},
+			},
+		},
+		{
+			"trailing commas",
+			2,
+			true,
+			[]string{"Header", "Reads", "Data", "Sequencing_Settings"},
+			[]int{4, 4, 5, 1},
+			[]SectionType{SettingsSection, SettingsSection, DataSection, SettingsSection},
+			[]byte(`[Header]
+FileFormatVersion,2
+RunName,TestRun
+InstrumentPlatform,NovaSeq
+InstrumentType,NovaSeq X Plus
+
+[Reads],
+Read1Cycles,151
+Read2Cycles,151
+Index1Cycles,8
+Index2Cycles,8
+
+[Data],,
+col1,col2,col3
+val1.1,val2.1,val3.1
+val1.2,val2.2,val3.2
+val1.3,val2.3,val3.3
+val1.4,val2.4,val3.4
+
+[Sequencing_Settings],
+LibraryPrepkits,prepkit1;prepkit2`),
+			map[string]map[string]string{
+				"Reads": {
+					"Read1Cycles":  "151",
+					"Read2Cycles":  "151",
+					"Index1Cycles": "8",
+					"Index2Cycles": "8",
+				},
+			},
+			map[string]map[string]map[int]string{
+				"Data": {
+					"col1": {
+						1: "val1.1",
+						2: "val1.2",
+						3: "val1.3",
+						4: "val1.4",
+					},
+					"col2": {
+						1: "val2.1",
+						2: "val2.2",
+						3: "val2.3",
+						4: "val2.4",
+					},
+					"col3": {
+						1: "val3.1",
+						2: "val3.2",
+						3: "val3.3",
+						4: "val3.4",
+					},
+				},
+			},
+		},
+		{
+			"empty values",
+			2,
+			true,
+			[]string{"Header", "Reads", "Data", "Sequencing_Settings"},
+			[]int{4, 4, 5, 1},
+			[]SectionType{SettingsSection, SettingsSection, DataSection, SettingsSection},
+			[]byte(`[Header]
+FileFormatVersion,2
+RunName,TestRun
+InstrumentPlatform,NovaSeq
+InstrumentType,NovaSeq X Plus
+
+[Reads],
+Read1Cycles,151
+Read2Cycles,151
+Index1Cycles,8
+Index2Cycles,8
+
+[Data],,
+col1,col2,col3
+val1.1,val2.1,val3.1
+val1.2,,val3.2
+val1.3,val2.3,
+val1.4,val2.4,val3.4
+
+[Sequencing_Settings],
+LibraryPrepkits,prepkit1;prepkit2`),
+			map[string]map[string]string{
+				"Reads": {
+					"Read1Cycles":  "151",
+					"Read2Cycles":  "151",
+					"Index1Cycles": "8",
+					"Index2Cycles": "8",
+				},
+			},
+			map[string]map[string]map[int]string{
+				"Data": {
+					"col1": {
+						1: "val1.1",
+						2: "val1.2",
+						3: "val1.3",
+						4: "val1.4",
+					},
+					"col2": {
+						1: "val2.1",
+						2: "",
+						3: "val2.3",
+						4: "val2.4",
+					},
+					"col3": {
+						1: "val3.1",
+						2: "val3.2",
+						3: "",
+						4: "val3.4",
+					},
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			r := bufio.NewReader(bytes.NewReader(c.Data))
+			s, err := ParseSampleSheet(r)
+			if err != nil {
+				t.Fatalf("%s", err)
+			}
+			if s.Version() != c.Version {
+				t.Errorf("expected version %d, found %d", c.Version, s.Version())
+			}
+			if len(s.Sections) != len(c.Sections) {
+				t.Errorf("expected %d sections, found %d", len(c.Sections), len(s.Sections))
+			}
+			if s.IsValid() != c.Valid {
+				t.Errorf("expected valid %t, found %t", c.Valid, s.IsValid())
+			}
+			for i, section := range c.Sections {
+				ssSection := s.Section(section)
+				if ssSection.Name != c.Sections[i] {
+					t.Errorf("expected section %s, found %s", c.Sections[i], ssSection.Name)
+				}
+				if ssSection.Type != c.SectionTypes[i] {
+					t.Errorf("expected section type %d, found %d", c.SectionTypes[i], ssSection.Type)
+				}
+				if len(ssSection.Rows) != c.SectionRows[i] {
+					t.Errorf("expected %d rows, found %d", c.SectionRows[i], len(ssSection.Rows))
+				}
+			}
+			for sName, sData := range c.ExpectedSettingValues {
+				for k, v := range sData {
+					observedValue, _ := s.Section(sName).Get(k)
+					if observedValue != v {
+						t.Errorf("expected %s %s to be %s, found %s", sName, k, v, observedValue)
+					}
+				}
+			}
+			for sName, sData := range c.ExpectedDataValues {
+				for k, indexes := range sData {
+					for i, v := range indexes {
+						observedValue, _ := s.Section(sName).Get(k, i)
+						if observedValue != v {
+							t.Errorf("expected %s %s[%d] to be %s, found %s", sName, k, i, v, observedValue)
+						}
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestMalformedSampleSheets(t *testing.T) {
+	cases := []struct {
+		Name  string
+		Data  []byte
+		Error string
+	}{
+		{
+			"trailing characters after header",
+			[]byte(`[Header] no text should be here`),
+			`parsing error: trailing characters after header "Header"`,
+		},
+		{
+			"no header at start of file",
+			[]byte(`key1,val1
+key2,val2`),
+			`parsing error: expected section header`,
+		},
+		{
+			"empty section",
+			[]byte(`[Header]
+[Reads]
+key1,val1
+key2,val2
+`),
+			`parsing error: empty section`,
+		},
+		{
+			"empty header",
+			[]byte(`[]`),
+			"parsing error: empty section header",
+		},
+		{
+			"differing number of items per row",
+			[]byte(`[Header]
+key1,val1
+key2
+`),
+			`parsing error: expected 2 items per row in section "Header"`,
+		},
+		{
+			"too many items for settings",
+			[]byte(`[Header]
+key1,val1,val2
+`),
+			`parsing error: expected 2 items per row in section "Header"`,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			r := bufio.NewReader(bytes.NewReader(c.Data))
+			s, err := ParseSampleSheet(r)
+			if err == nil {
+				t.Log(s)
+				t.Error("expected error, got nothing")
+			} else if err.Error() != c.Error {
+				t.Errorf("expected error: %q, got: %q", c.Error, err.Error())
+			}
+		})
+	}
+}
+
+func TestGettingNonexistentValues(t *testing.T) {
+	r := bufio.NewReader(bytes.NewReader([]byte(`[Header]
+key1,val1
+key2,val2
+`)))
+	s, err := ParseSampleSheet(r)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	v, ok := s.Section("Header").Get("key3")
+	if ok {
+		t.Error("expected not ok, got ok")
+	}
+	if v != "" {
+		t.Errorf("expected empty string, got %s", v)
+	}
+}
