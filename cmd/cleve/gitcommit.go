f9200954aef7029db678065b4d65247dbd049134
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-08-21 14:18:08 +0200

    Produce empty file if git describe fails

--- cmd/cleve/gitcommit.go
+++ cmd/cleve/gitcommit.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-//go:generate sh -c "git describe --tags > version.txt"
+//go:generate sh -c "git describe --tags > version.txt || echo '' > version.txt"
 //go:embed version.txt
 var Version string
 var LastRelease string = "v0.1.0" // x-release-please-version
