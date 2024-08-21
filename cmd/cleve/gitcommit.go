386f198848d3759f6ab7d4c0eaf416868c17924c
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-08-21 14:53:44 +0200

    fix: fallback version fail due to newline (#14)

--- cmd/cleve/gitcommit.go
+++ cmd/cleve/gitcommit.go
@@ -5,14 +5,15 @@ import (
 	"strings"
 )
 
-//go:generate sh -c "git describe --tags > version.txt || echo '' > version.txt"
+//go:generate sh -c "git describe --tags > version.txt || printf '' > version.txt"
 //go:embed version.txt
 var Version string
 var LastRelease string = "v0.1.0" // x-release-please-version
 
 func GetVersion() string {
-	if Version == "" {
+	v := strings.TrimSpace(Version)
+	if v == "" {
 		return LastRelease
 	}
-	return strings.TrimSpace(Version)
+	return v
 }
