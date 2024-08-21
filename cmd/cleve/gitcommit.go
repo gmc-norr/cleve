3efe6aeff1e89487d36898e759d33b5f75615f95
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-08-21 14:09:13 +0200

    fix: version fallback
    This is mainly for when downloading a tarball of the source and building
    manually. In those cases, the version file will be empty after go
    generate since it's not a git repo.

--- cmd/cleve/gitcommit.go
+++ cmd/cleve/gitcommit.go
@@ -1,7 +1,18 @@
 package main
 
-import _ "embed"
+import (
+	_ "embed"
+	"strings"
+)
 
-//go:generate sh -c "printf %s $(git describe --tags) > version.txt"
+//go:generate sh -c "git describe --tags > version.txt"
 //go:embed version.txt
 var Version string
+var LastRelease string = "v0.1.0" // x-release-please-version
+
+func GetVersion() string {
+	if Version == "" {
+		return LastRelease
+	}
+	return strings.TrimSpace(Version)
+}
