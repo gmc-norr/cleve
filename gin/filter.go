bd889d59039c39ee2e35604329bd5c7fb9a3d9bb
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-09-10 09:00:24 +0200

    refactor: QC filter reorganisation (#24)
    Refactor QC filter

--- gin/filter.go
+++ gin/filter.go
@@ -52,3 +52,23 @@ func getSampleFilter(c *gin.Context) (cleve.SampleFilter, error) {
 
 	return filter, nil
 }
+
+func getQcFilter(c *gin.Context) (cleve.QcFilter, error) {
+	filter := cleve.QcFilter{}
+
+	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+	if err != nil {
+		return filter, err
+	}
+	pageSize, err := strconv.Atoi(c.DefaultQuery("page_size", "10"))
+	if err != nil {
+		return filter, err
+	}
+
+	filter.RunID = c.Query("run_id")
+	filter.Platform = c.Query("platform")
+	filter.Page = page
+	filter.PageSize = pageSize
+
+	return filter, nil
+}
