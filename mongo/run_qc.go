1098405799eb78fc30c3a3d0d5d8f4d9ac69fac0
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2025-01-03 14:10:59 +0100

    Simplify QC query
    This makes it more similar to the query used for runs, and it will also
    generate the correct values for the pagination metadata, as well as
    returning and empty slice for the qc instead of nil.

--- mongo/run_qc.go
+++ mongo/run_qc.go
@@ -16,37 +16,12 @@ func (db DB) CreateRunQC(runId string, qc *cleve.InteropQC) error {
 }
 
 func (db DB) RunQCs(filter cleve.QcFilter) (cleve.QcResult, error) {
-	var qcPipeline mongo.Pipeline
-	var aggPipeline mongo.Pipeline
+	var pipeline mongo.Pipeline
 	var qc cleve.QcResult
-
-	aggPipeline = append(aggPipeline, bson.D{
-		{Key: "$lookup", Value: bson.M{
-			"from":         "runs",
-			"localField":   "run_id",
-			"foreignField": "run_id",
-			"as":           "run",
-		}},
-	})
-
-	// We expect only one run, so unwind the array
-	qcPipeline = append(qcPipeline, bson.D{
-		{Key: "$unwind", Value: "$run"},
-	})
-
-	// Platform filter
-	if filter.Platform != "" {
-		platformFilter := bson.D{
-			{Key: "$match", Value: bson.D{
-				{Key: "run.platform", Value: filter.Platform},
-			}},
-		}
-		aggPipeline = append(aggPipeline, platformFilter)
-	}
 
 	// Run ID filter
 	if filter.RunIdQuery != "" {
-		runIdFilter := bson.D{
+		pipeline = append(pipeline, bson.D{
 			{Key: "$match", Value: bson.D{
 				{Key: "$expr", Value: bson.D{
 					{Key: "$regexMatch", Value: bson.M{
@@ -56,98 +31,132 @@ func (db DB) RunQCs(filter cleve.QcFilter) (cleve.QcResult, error) {
 					}},
 				}},
 			}},
-		}
-		aggPipeline = append(aggPipeline, runIdFilter)
+		})
 	}
+
+	// Get run information
+	pipeline = append(pipeline, bson.D{
+		{Key: "$lookup", Value: bson.M{
+			"from":         "runs",
+			"localField":   "run_id",
+			"foreignField": "run_id",
+			"as":           "run",
+		}},
+	})
+
+	// We expect only one run, so unwind the array
+	pipeline = append(pipeline, bson.D{
+		{Key: "$unwind", Value: "$run"},
+	})
+
+	// Platform filter
+	if filter.Platform != "" {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$match", Value: bson.D{
+				{Key: "run.platform", Value: filter.Platform},
+			}},
+		})
+	}
+
+	qcFacet := mongo.Pipeline{}
 
 	// Skip
 	if filter.Page > 0 && filter.PageSize > 0 {
-		qcPipeline = append(qcPipeline, bson.D{
+		qcFacet = append(qcFacet, bson.D{
 			{Key: "$skip", Value: filter.PageSize * (filter.Page - 1)},
 		})
 	}
 
 	// Limit
 	if filter.PageSize > 0 {
-		qcPipeline = append(qcPipeline, bson.D{
+		qcFacet = append(qcFacet, bson.D{
 			{Key: "$limit", Value: filter.PageSize},
 		})
 	}
 
 	// Sort
-	qcPipeline = append(qcPipeline, bson.D{
+	qcFacet = append(qcFacet, bson.D{
 		{Key: "$sort", Value: bson.D{
 			{Key: "run.run_info.run.date", Value: -1},
 		}},
 	})
 
 	// Facet to get both metadata and qc
-	aggPipeline = append(aggPipeline, bson.D{
+	pipeline = append(pipeline, bson.D{
 		{Key: "$facet", Value: bson.M{
 			"metadata": bson.A{
 				bson.D{{Key: "$count", Value: "total_count"}},
 			},
-			"qc": qcPipeline,
+			"qc": qcFacet,
 		}},
 	})
 
-	aggPipeline = append(aggPipeline, bson.D{
-		{Key: "$unwind", Value: "$metadata"},
+	pipeline = append(pipeline, bson.D{
+		{
+			Key: "$project",
+			Value: bson.M{
+				"metadata": bson.M{
+					"$arrayElemAt": bson.A{"$metadata", 0},
+				},
+				"qc": 1,
+			},
+		},
 	})
 
-	aggPipeline = append(aggPipeline, bson.D{
-		{Key: "$set", Value: bson.D{
-			{Key: "metadata.count", Value: bson.D{
-				{Key: "$size", Value: "$qc"},
-			}},
-			{Key: "metadata.page", Value: filter.Page},
-			{Key: "metadata.page_size", Value: filter.PageSize},
-		}},
-	})
-
-	if filter.PageSize > 0 {
-		aggPipeline = append(aggPipeline, bson.D{
-			{Key: "$set", Value: bson.D{
-				{Key: "metadata.total_pages", Value: bson.D{
-					{
-						Key: "$ceil", Value: bson.D{
-							{Key: "$divide", Value: bson.A{"$metadata.total_count", filter.PageSize}},
+	pipeline = append(pipeline, bson.D{
+		{
+			Key: "$set",
+			Value: bson.M{
+				"metadata.count": bson.M{
+					"$size": "$qc",
+				},
+				"metadata.page":      filter.Page,
+				"metadata.page_size": filter.PageSize,
+				"metadata.total_pages": bson.M{
+					"$cond": bson.M{
+						"if": bson.M{
+							"$gt": bson.A{
+								filter.PageSize,
+								0,
+							},
 						},
+						"then": bson.M{
+							"$ceil": bson.M{
+								"$divide": bson.A{
+									"$metadata.total_count",
+									filter.PageSize,
+								},
+							},
+						},
+						"else": 1,
 					},
-				}},
-			}},
-		})
-	} else {
-		aggPipeline = append(aggPipeline, bson.D{
-			{Key: "$set", Value: bson.D{
-				{Key: "metadata.total_pages", Value: 1},
-				{Key: "metadata.page_size", Value: "$metadata.total_count"},
-			}},
-		})
-	}
+				},
+			},
+		},
+	})
 
-	cursor, err := db.RunQCCollection().Aggregate(context.TODO(), aggPipeline)
+	cursor, err := db.RunQCCollection().Aggregate(context.TODO(), pipeline)
 	if err != nil {
-		return qc, err
+		return cleve.QcResult{}, err
+	}
+	defer cursor.Close(context.TODO())
+
+	cursor.Next(context.TODO())
+	err = cursor.Decode(&qc)
+	if err != nil {
+		return cleve.QcResult{}, err
+	}
+	if qc.TotalCount == 0 {
+		qc.TotalPages = 1
+	}
+	if qc.Page > qc.TotalPages {
+		return qc, PageOutOfBoundsError{
+			page:       qc.Page,
+			totalPages: qc.TotalPages,
+		}
 	}
 
-	if cursor.Next(context.TODO()) {
-		err = cursor.Decode(&qc)
-		if err != nil {
-			return qc, err
-		}
-		if qc.TotalCount == 0 {
-			qc.TotalPages = 1
-		}
-		if qc.Page > qc.TotalPages {
-			return qc, PageOutOfBoundsError{
-				page:       qc.Page,
-				totalPages: qc.TotalPages,
-			}
-		}
-	}
-
-	return qc, cursor.Err()
+	return qc, nil
 }
 
 func (db DB) RunQC(runId string) (*cleve.InteropQC, error) {
