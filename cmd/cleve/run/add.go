b9ed6677020466b06e1b8ff03c4cd85a970702d9
Niklas Mähler <niklas.mahler@gmail.com>
2025-03-22 15:52:27 +0100

    CLI flag for setting run state when adding run

--- cmd/cleve/run/add.go
+++ cmd/cleve/run/add.go
@@ -12,72 +12,75 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var addCmd = &cobra.Command{
-	Use:   "add [flags] run_directory",
-	Short: "Add a sequencing run",
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return fmt.Errorf("run directory is required")
-		}
-		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		db, err := mongo.Connect()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		runDir, err := filepath.Abs(args[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		interopData, err := interop.InteropFromDir(runDir)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		sampleSheetFile, err := cleve.MostRecentSamplesheet(runDir)
-		if err != nil {
-			if err.Error() == "no samplesheet found" {
-				log.Printf("no samplesheet found for run")
-			} else {
-				log.Fatal(err)
+var (
+	runState cleve.RunState
+	addCmd   = &cobra.Command{
+		Use:   "add [flags] run_directory",
+		Short: "Add a sequencing run",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("run directory is required")
 			}
-		}
-
-		var state cleve.RunState
-		if err = state.Set("pending"); err != nil {
-			log.Fatal(err)
-		}
-
-		if sampleSheetFile != "" {
-			log.Printf("most recent samplesheet: %s", sampleSheetFile)
-			samplesheet, err := cleve.ReadSampleSheet(sampleSheetFile)
+			state, _ := cmd.Flags().GetString("state")
+			return runState.Set(state)
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			db, err := mongo.Connect()
 			if err != nil {
 				log.Fatal(err)
 			}
-			_, err = db.CreateSampleSheet(samplesheet, mongo.SampleSheetWithRunId(interopData.RunInfo.RunId))
+
+			runDir, err := filepath.Abs(args[0])
 			if err != nil {
 				log.Fatal(err)
 			}
-		}
 
-		run := cleve.Run{
-			RunID:          interopData.RunInfo.RunId,
-			ExperimentName: interopData.RunParameters.ExperimentName,
-			Path:           runDir,
-			Platform:       interopData.RunInfo.Platform,
-			RunParameters:  interopData.RunParameters,
-			RunInfo:        interopData.RunInfo,
-			StateHistory:   []cleve.TimedRunState{{State: state, Time: time.Now()}},
-			Analysis:       []*cleve.Analysis{},
-		}
+			interopData, err := interop.InteropFromDir(runDir)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		if err = db.CreateRun(&run); err != nil {
-			log.Fatal(err)
-		}
+			sampleSheetFile, err := cleve.MostRecentSamplesheet(runDir)
+			if err != nil {
+				if err.Error() == "no samplesheet found" {
+					log.Printf("no samplesheet found for run")
+				} else {
+					log.Fatal(err)
+				}
+			}
 
-		log.Printf("Successfully added run %s", run.RunID)
-	},
+			if sampleSheetFile != "" {
+				log.Printf("most recent samplesheet: %s", sampleSheetFile)
+				samplesheet, err := cleve.ReadSampleSheet(sampleSheetFile)
+				if err != nil {
+					log.Fatal(err)
+				}
+				_, err = db.CreateSampleSheet(samplesheet, mongo.SampleSheetWithRunId(interopData.RunInfo.RunId))
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			run := cleve.Run{
+				RunID:          interopData.RunInfo.RunId,
+				ExperimentName: interopData.RunParameters.ExperimentName,
+				Path:           runDir,
+				Platform:       interopData.RunInfo.Platform,
+				RunParameters:  interopData.RunParameters,
+				RunInfo:        interopData.RunInfo,
+				StateHistory:   []cleve.TimedRunState{{State: runState, Time: time.Now()}},
+				Analysis:       []*cleve.Analysis{},
+			}
+
+			if err = db.CreateRun(&run); err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Successfully added run %s", run.RunID)
+		},
+	}
+)
+
+func init() {
+	addCmd.Flags().StringP("state", "s", "pending", "state of the run")
 }
