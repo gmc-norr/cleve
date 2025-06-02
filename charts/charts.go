e1f95c1401f1adb479ba3404b28ac7002f16886d
Niklas Mähler <niklas.mahler@gmail.com>
2025-06-02 09:26:09 +0200

    Keep ordering of scatter plot series consistent
    I did this by making the scatter data into a generic type constrained on
    the group field that has to implement the cmp.Ordered interface. This is
    not a great solution, at least in terms of ergonomics, but I couldn't
    really find a better way of doing it. I'll keep it for now since it
    seems to be working fine.

--- charts/charts.go
+++ charts/charts.go
@@ -1,7 +1,9 @@
 package charts
 
 import (
+	"cmp"
 	"fmt"
+	"slices"
 
 	"github.com/gmc-norr/cleve/interop"
 	"github.com/go-echarts/go-echarts/v2/charts"
@@ -36,8 +38,10 @@ func (s RunStats[T]) Plot() (render.Renderer, error) {
 	}
 }
 
-type ScatterData struct {
-	Data     []ScatterDatum
+// ScatterData represents the a dataset for a scatter chart. The generic type T
+// represents the type of the Group field for the individual data points.
+type ScatterData[T cmp.Ordered] struct {
+	Data     []ScatterDatum[T]
 	XLabel   string
 	YLabel   string
 	XLimit   [2]float64
@@ -45,13 +49,15 @@ type ScatterData struct {
 	Grouping string
 }
 
-type ScatterDatum struct {
+// ScatterDatum represents a single data point for ScatterData. The generic type T
+// represents the type of the Group field.
+type ScatterDatum[T cmp.Ordered] struct {
 	X     float64
 	Y     float64
-	Color any
+	Group T
 }
 
-func (d ScatterData) Plot() (render.Renderer, error) {
+func (d ScatterData[T]) Plot() (render.Renderer, error) {
 	return ScatterChart(d), nil
 }
 
@@ -95,7 +101,7 @@ func BarChart[T interop.OptionalFloat | float64 | int](d RunStats[T]) *charts.Ba
 	return chart
 }
 
-func ScatterChart(d ScatterData) *charts.Scatter {
+func ScatterChart[T cmp.Ordered](d ScatterData[T]) *charts.Scatter {
 	chart := charts.NewScatter()
 	xOpts := opts.XAxis{Name: d.XLabel}
 	if d.XLimit[0] != d.XLimit[1] {
@@ -115,6 +121,7 @@ func ScatterChart(d ScatterData) *charts.Scatter {
 		charts.WithDataZoomOpts(opts.DataZoom{Orient: "vertical", Type: "slider"}),
 		charts.WithDataZoomOpts(opts.DataZoom{Orient: "horizontal", Type: "slider"}),
 	)
+	labels := make([]T, 0)
 	series := make(map[any][]opts.ScatterData)
 	for _, k := range d.Data {
 		sd := opts.ScatterData{
@@ -122,11 +129,16 @@ func ScatterChart(d ScatterData) *charts.Scatter {
 			Symbol:     "circle",
 			SymbolSize: 5,
 		}
-		series[k.Color] = append(series[k.Color], sd)
+		if !slices.Contains(labels, k.Group) {
+			labels = append(labels, k.Group)
+		}
+		series[k.Group] = append(series[k.Group], sd)
 	}
 	chart.SetXAxis(nil)
-	for k, v := range series {
-		chart.AddSeries(fmt.Sprintf("%v", k), v)
+	slices.Sort(labels)
+
+	for _, k := range labels {
+		chart.AddSeries(fmt.Sprintf("%v", k), series[k])
 	}
 	return chart
 }
--- gin/run_charts.go
+++ gin/run_charts.go
@@ -81,7 +81,7 @@ func RunChartsHandler(db RunQCGetter) gin.HandlerFunc {
 			return
 		}
 
-		plotData := charts.ScatterData{
+		plotData := charts.ScatterData[int]{
 			XLimit: [2]float64{0, 100},
 			YLimit: [2]float64{0, 100},
 		}
@@ -106,7 +106,7 @@ func RunChartsHandler(db RunQCGetter) gin.HandlerFunc {
 		}
 
 		for _, tile := range qc.TileSummary {
-			d := charts.ScatterDatum{}
+			d := charts.ScatterDatum[int]{}
 			switch config.XData {
 			case "percent_occupied":
 				d.X = float64(tile.PercentOccupied)
@@ -122,7 +122,7 @@ func RunChartsHandler(db RunQCGetter) gin.HandlerFunc {
 
 			switch config.ColorBy {
 			case "lane":
-				d.Color = tile.Lane
+				d.Group = tile.Lane
 			}
 
 			plotData.Data = append(plotData.Data, d)
