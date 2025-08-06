fc466d3ee9bbf31565caa5ea6c9801046e5e6bb9
Niklas Mähler <niklas.mahler@gmail.com>
2025-08-06 11:04:06 +0200

    Revamped parsing of run completion status
    This now takes all the different formats into account in a better way
    than before, and MiSeq has also been added.

--- runcompletionstatus_test.go
+++ runcompletionstatus_test.go
@@ -1,55 +1,95 @@
 package cleve
 
 import (
-	"errors"
-	"os"
 	"testing"
 )
 
-func TestReadRunCompletionStatus(t *testing.T) {
+func TestParseRunCompletionStatus(t *testing.T) {
 	cases := []struct {
-		name     string
-		filename string
-		status   string
-		message  string
+		name    string
+		xml     []byte
+		error   bool
+		success bool
+		message string
 	}{
 		{
-			"novaseq",
-			"/home/nima18/git/cleve/test_data/novaseq_full/RunCompletionStatus.xml",
-			"success",
-			"RunCompleted",
+			name:    "novaseq success",
+			xml:     []byte(`<?xml version="1.0" encoding="utf-8"?><RunCompletionStatus><RunStatus>RunCompleted</RunStatus></RunCompletionStatus>`),
+			success: true,
+			message: "RunCompleted",
 		},
 		{
-			"nextseq1",
-			"/home/nima18/git/cleve/test_data/nextseq1_full/RunCompletionStatus.xml",
-			"success",
-			"CompletedAsPlanned",
+			name: "novaseq error",
+			xml: []byte(`<?xml version="1.0" encoding="utf-8"?>
+				<RunCompletionStatus>
+					<RunStatus>RunErrored</RunStatus>
+					<RunError>
+						<Type>Illumina.Instrument.Recipe.Runtime.Exceptions.RecipeExecutionFailedException</Type>
+						<Message>I/O data integrity check failed</Message>
+					</RunError>
+				</RunCompletionStatus>`),
+			success: false,
+			message: "RunErrored: Illumina.Instrument.Recipe.Runtime.Exceptions.RecipeExecutionFailedException: I/O data integrity check failed",
 		},
 		{
-			"nextseq2",
-			"/home/nima18/git/cleve/test_data/nextseq2_full/RunCompletionStatus.xml",
-			"success",
-			"CompletedAsPlanned",
+			name: "nextseq success",
+			xml: []byte(`<?xml version="1.0"?>
+				<RunCompletionStatus>
+					<CompletionStatus>CompletedAsPlanned</CompletionStatus>
+					<ErrorDescription>None</ErrorDescription>
+				</RunCompletionStatus>`),
+			success: true,
+			message: "CompletedAsPlanned",
 		},
 		{
-			"nextseq_failed",
-			"/home/nima18/git/cleve/test_data/230713_NB551119_0374_AHMVKTBGXT/RunCompletionStatus.xml",
-			"error",
-			"Thread was being aborted. (System.Threading.ThreadAbortException)",
+			name: "nextseq error",
+			xml: []byte(`<?xml version="1.0"?>
+				<RunCompletionStatus>
+					<CompletionStatus>UserEndedEarly</CompletionStatus>
+					<ErrorDescription>Thread was aborted</ErrorDescription>
+				</RunCompletionStatus>`),
+			success: false,
+			message: "UserEndedEarly: Thread was aborted",
+		},
+		{
+			name:    "miseq success",
+			xml:     []byte(`<?xml version="1.0"?><AnalysisJobInfo></AnalysisJobInfo>`),
+			success: true,
+			message: "Success",
+		},
+		{
+			name:    "miseq error",
+			xml:     []byte(`<?xml version="1.0"?><AnalysisJobInfo><Error>Sequencing failed</Error></AnalysisJobInfo>`),
+			success: false,
+			message: "Error: Sequencing failed",
+		},
+		{
+			name:    "miseq warning",
+			xml:     []byte(`<?xml version="1.0"?><AnalysisJobInfo><Warning>Stuff went wrong</Warning></AnalysisJobInfo>`),
+			success: false,
+			message: "Warning: Stuff went wrong",
+		},
+		{
+			name:    "miseq error and warning",
+			xml:     []byte(`<?xml version="1.0"?><AnalysisJobInfo><Error>Sequencing failed</Error><Warning>Stuff went wrong</Warning></AnalysisJobInfo>`),
+			success: false,
+			message: "Error: Sequencing failed, Warning: Stuff went wrong",
+		},
+		{
+			name:  "invalid document",
+			xml:   []byte(`<?xml version="1.0"?><OtherCompletionStatus><Error>Sequencing failed</Error><Warning>Stuff went wrong</Warning></OtherCompletionStatus>`),
+			error: true,
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			if _, err := os.Stat(c.filename); errors.Is(err, os.ErrNotExist) {
-				t.Skip("test data not found, skipping")
-			}
-			rct, err := ReadRunCompletionStatus(c.filename)
-			if err != nil {
+			rct, err := ParseRunCompletionStatus(c.xml)
+			if c.error != (err != nil) {
 				t.Fatal(err.Error())
 			}
-			if c.status != rct.Status {
-				t.Errorf(`expected status "%s", got "%s"`, c.status, rct.Status)
+			if c.success != rct.Success {
+				t.Errorf(`expected success to be %t, got %t`, c.success, rct.Success)
 			}
 			if c.message != rct.Message {
 				t.Errorf(`expected message "%s", got "%s"`, c.message, rct.Message)
