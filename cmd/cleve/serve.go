d35bce84117d840bba9abca386ac547e006ddbbe
Niklas Mähler <niklas.mahler@gmail.com>
2025-08-08 14:14:20 +0200

    Start run watcher with server

--- cmd/cleve/serve.go
+++ cmd/cleve/serve.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gmc-norr/cleve/gin"
 	"github.com/gmc-norr/cleve/mongo"
+	"github.com/gmc-norr/cleve/watcher"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -27,9 +33,34 @@ var (
 			host := viper.GetString("host")
 			port := viper.GetInt("port")
 			addr := fmt.Sprintf("%s:%d", host, port)
+
+			loglevel := slog.LevelWarn
+			if debug {
+				loglevel = slog.LevelDebug
+			}
+			logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: loglevel}))
+			slog.SetDefault(logger)
+
+			runWatcher := watcher.NewRunWatcher(10*time.Second, db, logger)
+			defer runWatcher.Stop()
+			runWatcher.Start()
+
+			interrupt := make(chan os.Signal, 1)
+			signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+			go func() {
+				s := <-interrupt
+				slog.Error("signal received, shutting down", "signal", s)
+				runWatcher.Stop()
+				os.Exit(1)
+			}()
+
 			router := gin.NewRouter(db, debug)
-			log.Printf("Serving on %s", addr)
-			log.Fatal(http.ListenAndServe(addr, router))
+			logger.Info("serving cleve", "address", addr)
+			err = http.ListenAndServe(addr, router)
+			if err != nil {
+				slog.Error("server crashed", "error", err)
+				os.Exit(1)
+			}
 		},
 	}
 )
