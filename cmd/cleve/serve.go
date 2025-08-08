7431351ab2c0b486536daad768951b3a0b1e042d
Niklas Mähler <niklas.mahler@gmail.com>
2025-08-08 14:27:29 +0200

    Ignore errors that can't happen

--- cmd/cleve/serve.go
+++ cmd/cleve/serve.go
@@ -70,13 +70,7 @@ func init() {
 	serveCmd.Flags().StringVar(&host, "host", "localhost", "host")
 	serveCmd.Flags().IntVarP(&port, "port", "p", 8080, "port")
 	serveCmd.Flags().StringVar(&logfile, "logfile", "", "file to write logs in")
-	if err := viper.BindPFlag("host", serveCmd.Flags().Lookup("host")); err != nil {
-		log.Fatal(err)
-	}
-	if err := viper.BindPFlag("port", serveCmd.Flags().Lookup("port")); err != nil {
-		log.Fatal(err)
-	}
-	if err := viper.BindPFlag("logfile", serveCmd.Flags().Lookup("logfile")); err != nil {
-		log.Fatal(err)
-	}
+	_ = viper.BindPFlag("host", serveCmd.Flags().Lookup("host"))
+	_ = viper.BindPFlag("port", serveCmd.Flags().Lookup("port"))
+	_ = viper.BindPFlag("logfile", serveCmd.Flags().Lookup("logfile"))
 }
