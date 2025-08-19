d6e0e9c4e4c2b8ce0cd2de8a72a4d74f0bf5a9b0
Niklas Mähler <niklas.mahler@gmail.com>
2025-08-19 10:22:59 +0200

    Update mongo analyses function

--- mongo/analysis.go
+++ mongo/analysis.go
@@ -8,16 +8,137 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (db DB) Analyses(runId string) ([]*cleve.Analysis, error) {
-	run, err := db.Run(runId, false)
-	if err != nil {
-		return nil, err
+func (db DB) Analyses(filter cleve.AnalysisFilter) (cleve.AnalysisResult, error) {
+	var (
+		analyses cleve.AnalysisResult
+		pipeline mongo.Pipeline
+	)
+
+	analyses.PaginationMetadata = cleve.PaginationMetadata{
+		Page:     filter.Page,
+		PageSize: filter.PageSize,
 	}
 
-	analyses := run.Analysis
-	if analyses == nil {
-		return []*cleve.Analysis{}, nil
+	if filter.AnalysisId != "" {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$match", Value: bson.D{
+				{Key: "analysis_id", Value: filter.AnalysisId},
+			}},
+		})
 	}
+
+	if filter.ParentId != "" {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$match", Value: bson.D{
+				{Key: "parent_id", Value: filter.ParentId},
+			}},
+		})
+	}
+
+	if filter.Software != "" {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$match", Value: bson.D{
+				{Key: "software", Value: filter.Software},
+			}},
+		})
+	}
+
+	pipeline = append(pipeline, bson.D{
+		{Key: "$set", Value: bson.D{
+			{Key: "state_history", Value: bson.D{
+				{Key: "$sortArray", Value: bson.M{
+					"input": "$state_history", "sortBy": bson.D{{Key: "time", Value: -1}},
+				}},
+			}},
+		}},
+	})
+
+	if filter.State != nil {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$match", Value: bson.D{
+				{Key: "$expr", Value: bson.D{
+					{Key: "$eq", Value: bson.A{
+						bson.D{{Key: "$arrayElemAt", Value: bson.A{"$state_history.state", 0}}},
+						filter.State,
+					}},
+				}},
+			}},
+		})
+	}
+
+	if filter.Level != nil {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$match", Value: bson.D{
+				{Key: "level", Value: filter.Level},
+			}},
+		})
+	}
+
+	metaPipeline := append(pipeline, bson.D{
+		{Key: "$count", Value: "total_count"},
+	})
+
+	cursor, err := db.AnalysesCollection().Aggregate(context.TODO(), metaPipeline)
+	if err != nil {
+		return analyses, err
+	}
+	defer closeCursor(cursor, context.TODO())
+
+	if !cursor.Next(context.TODO()) {
+		analyses.TotalCount = 0
+	}
+	if err := cursor.Decode(&analyses.PaginationMetadata); analyses.TotalCount > 0 && err != nil {
+		return analyses, err
+	}
+
+	if analyses.PageSize > 0 {
+		analyses.TotalPages = analyses.TotalCount / analyses.PageSize
+		if analyses.TotalCount%analyses.PageSize > 0 {
+			analyses.TotalPages += 1
+		}
+	} else {
+		analyses.TotalPages = 1
+	}
+
+	if filter.Page > 0 {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$skip", Value: filter.PageSize * (filter.Page - 1)},
+		})
+	}
+
+	if filter.PageSize > 0 {
+		pipeline = append(pipeline, bson.D{
+			{Key: "$limit", Value: filter.PageSize},
+		})
+	}
+
+	cursor, err = db.AnalysesCollection().Aggregate(context.TODO(), pipeline)
+	if err != nil {
+		return analyses, err
+	}
+	defer closeCursor(cursor, context.TODO())
+
+	for cursor.Next(context.TODO()) {
+		var analysis cleve.Analysis
+		err = cursor.Decode(&analysis)
+		if err != nil {
+			return analyses, err
+		}
+		analyses.Count += 1
+		analyses.Analyses = append(analyses.Analyses, &analysis)
+	}
+
+	if analyses.TotalCount == 0 {
+		analyses.TotalPages = 1
+		analyses.Analyses = make([]*cleve.Analysis, 0)
+	}
+	if analyses.Page > analyses.TotalPages {
+		return analyses, PageOutOfBoundsError{
+			page:       analyses.Page,
+			totalPages: analyses.TotalPages,
+		}
+	}
+
 	return analyses, nil
 }
 
