d611838222648046294adc291cc1a4659133eda8
Niklas Mähler <niklas.mahler@gmail.com>
2026-02-09 16:18:48 +0100

    Add failing test for updating analysis files

--- gin/analysis_test.go
+++ gin/analysis_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gmc-norr/cleve"
@@ -63,6 +64,104 @@ func TestAddAnalysis(t *testing.T) {
 
 			if w.Code != c.code {
 				t.Errorf("expected status code %d, got %d: %s", c.code, w.Code, w.Body)
+			}
+		})
+	}
+}
+
+func TestUpdateAnalysis(t *testing.T) {
+	gin.SetMode("test")
+	testcases := []struct {
+		name             string
+		existingAnalysis *cleve.Analysis
+		exists           bool
+		pathUpdated      bool
+		stateUpdated     bool
+		filesUpdated     bool
+		data             []byte
+		code             int
+	}{
+		{
+			name: "update analysis state",
+			existingAnalysis: &cleve.Analysis{
+				AnalysisId:   "analysis1",
+				Path:         "/path/to/analysis1",
+				StateHistory: cleve.StateHistory{cleve.TimedRunState{State: cleve.StatePending, Time: time.Now()}},
+			},
+			exists:       true,
+			stateUpdated: true,
+			data:         []byte(`{"state": "ready"}`),
+			code:         http.StatusOK,
+		},
+		{
+			name: "update analysis path",
+			existingAnalysis: &cleve.Analysis{
+				AnalysisId:   "analysis1",
+				Path:         "/path/to/analysis1",
+				StateHistory: cleve.StateHistory{cleve.TimedRunState{State: cleve.StatePending, Time: time.Now()}},
+			},
+			exists:      true,
+			pathUpdated: true,
+			data:        []byte(`{"path": "/new/path/to/analysis1"}`),
+			code:        http.StatusOK,
+		},
+		{
+			name: "update analysis files (relative paths)",
+			existingAnalysis: &cleve.Analysis{
+				AnalysisId:   "analysis1",
+				Path:         "/path/to/analysis1",
+				StateHistory: cleve.StateHistory{cleve.TimedRunState{State: cleve.StatePending, Time: time.Now()}},
+			},
+			exists:       true,
+			filesUpdated: true,
+			data:         []byte(`{"files": [{"path": "output/sample1.data", "level": "sample", "parent_id": "analysis1"}]}`),
+			code:         http.StatusOK,
+		},
+	}
+
+	for _, c := range testcases {
+		t.Run(c.name, func(t *testing.T) {
+			gs := mock.AnalysisGetterSetter{}
+			gs.AnalysisFn = func(s1 string, s2 ...string) (*cleve.Analysis, error) {
+				if c.exists {
+					return nil, mongo.ErrNoDocuments
+				}
+				return &cleve.Analysis{}, nil
+			}
+			gs.SetAnalysisPathFn = func(string, string) error {
+				return nil
+			}
+			gs.SetAnalysisStateFn = func(string, cleve.State) error {
+				return nil
+			}
+			gs.SetAnalysisFilesFn = func(string, []cleve.AnalysisFile) error {
+				return nil
+			}
+			gs.CreateAnalysisFn = func(a *cleve.Analysis) error {
+				return nil
+			}
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+
+			ctx.Request = httptest.NewRequest(http.MethodPatch, "/analyses/analysis_1", bytes.NewBuffer(c.data))
+			ctx.Request.Header.Set("Content-Type", "application/json")
+
+			UpdateAnalysisHandler(&gs)(ctx)
+
+			if w.Code != c.code {
+				t.Errorf("expected status code %d, got %d: %s", c.code, w.Code, w.Body)
+			}
+
+			if c.stateUpdated != gs.SetAnalysisStateInvoked {
+				t.Errorf("state updated: %t, expected it: %t", gs.SetAnalysisStateInvoked, c.stateUpdated)
+			}
+
+			if c.pathUpdated != gs.SetAnalysisPathInvoked {
+				t.Errorf("state updated: %t, expected it: %t", gs.SetAnalysisPathInvoked, c.pathUpdated)
+			}
+
+			if c.filesUpdated != gs.SetAnalysisFilesInvoked {
+				t.Errorf("state updated: %t, expected it: %t", gs.SetAnalysisFilesInvoked, c.filesUpdated)
 			}
 		})
 	}
